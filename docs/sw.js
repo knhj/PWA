b12de5b27d8557419e947e7682b08c15a77f42da
kano hajime <knhj.algorithm@gmail.com>
2019-03-31 14:42:08 +0900

    12

--- docs/sw.js
+++ docs/sw.js
@@ -1,17 +1,16 @@
-//キャッシュ名
+
 var CACHE_NAME = 'cache-v1';
 
-//キャッシュに入れるリソースのパス
 var urlsToCache = [
     '/',
     '<画像ファイル>'
 ];
 
-//インストール状態のイベント処理
+
 self.addEventListener('install', function (event) {
     event.waitUntil(
 
-        //キャッシュの中に必要なリソースを格納する
+
         caches.open(CACHE_NAME).then(function (cache) {
 
             return cache.addAll(urlsToCache);
@@ -21,13 +20,12 @@ self.addEventListener('install', function (event) {
     );
 });
 
-//有効化状態のイベント処理
 self.addEventListener('activate', function (event) {
     event.waitUntil(
 
-        //現在のキャッシュをすべて取得する
+
         caches.keys().then(function (cache) {
-            //新しいキャッシュ以外は削除する
+
             cache.map(function (name) {
                 if (CACHE_NAME !== name) caches.delete(name);
             })
@@ -36,11 +34,11 @@ self.addEventListener('activate', function (event) {
     );
 });
 
-//リクエスト取得状態のイベント処理
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(
 
-        //リクエストに応じたリソースがキャッシュにあればそれを使う
+
         caches.match(event.request).then(function (res) {
             if (res) return res;
 
